ed68b6dc45700c7653ec9ade43631301b8f9a48c
mohamedosama25 <147179514+mohamedosama25@users.noreply.github.com>
2024-07-29 14:25:19 +0300

    Luxor done
    Added images and changed the quote

--- js/luxor.js
+++ js/luxor.js
@@ -2,7 +2,7 @@ places = [
     {
         "id": 39,
         "name": "Karnak Temple",
-        "imagesrc": "",
+        "imagesrc": "https://cdn.getyourguide.com/img/location/5963844562868.jpeg/88.jpg",
         "description": "Karnak Temple is a vast temple complex dedicated to the god Amun, located near Luxor, Egypt. This impressive site features massive pylons, towering obelisks, and intricately designed sanctuaries. The temple complex was built over centuries by numerous pharaohs, making it a historical treasure trove. Visitors can explore the Great Hypostyle Hall, famous for its forest of giant columns. Karnak Temple is also home to the Sacred Lake and several smaller temples and chapels. The complex is a testament to ancient Egyptian architecture and religious practices. Sound and light shows held in the evenings provide a magical experience, bringing the history of the temple to life.",
         "price": {
             "Entrance Fee": {
@@ -27,7 +27,7 @@ places = [
     {
         "id": 40,
         "name": "Luxor Temple",
-        "imagesrc": "",
+        "imagesrc": "https://e0.pxfuel.com/wallpapers/750/767/desktop-wallpaper-luxor-luxor-las-vegas.jpg",
         "description": "Luxor Temple is a large ancient Egyptian temple complex situated on the east bank of the Nile River in Luxor. It is renowned for its grand statues, sphinxes, and beautiful reliefs. The temple was primarily built by Amenhotep III and later completed by Tutankhamun and Ramses II. Luxor Temple was dedicated to the Theban Triad of Amun, Mut, and Khonsu. The site is especially picturesque at night when it is illuminated, creating a stunning visual effect. The Avenue of Sphinxes, which once connected Luxor Temple to Karnak Temple, is being excavated and restored. The temple's architecture showcases the grandeur of ancient Egyptian civilization. The annual Opet Festival was celebrated here in ancient times, emphasizing its religious significance.",
         "price": {
             "Entrance Fee": {
@@ -52,7 +52,7 @@ places = [
     {
         "id": 41,
         "name": "Valley of the Kings",
-        "imagesrc": "",
+        "imagesrc": "https://www.historyhit.com/app/uploads/fly-images/5166545/shutterstock_valley-of-kings-788x537.jpg",
         "description": "The Valley of the Kings is a royal burial ground for pharaohs and nobles, located on the west bank of the Nile near Luxor. It is renowned for its elaborately decorated tombs, including the famous tomb of Tutankhamun. The valley contains 63 tombs and chambers, showcasing intricate hieroglyphs and stunning wall paintings. Each tomb provides unique insights into ancient Egyptian beliefs about the afterlife. Visitors can explore several tombs, each with its distinct features and artwork. The valley's isolation and hidden location were intended to protect the tombs from looters. Despite this, many tombs were plundered in antiquity. Today, it remains a major archaeological and tourist site, attracting researchers and history enthusiasts worldwide.",
         "price": {
             "Entrance Fee": {
@@ -77,7 +77,7 @@ places = [
     {
         "id": 42,
         "name": "Valley of the Queens",
-        "imagesrc": "",
+        "imagesrc": "https://www.touregyptclub.com/wp-content/uploads/2020/07/e6324_Valley-of-the-queens-temple.jpg",
         "description": "The Valley of the Queens is the burial site of the wives of pharaohs, located near the Valley of the Kings in Luxor. This serene and beautiful valley is home to around 90 tombs, each with intricate decorations and inscriptions. The tombs of the queens and their children feature stunning wall paintings and well-preserved reliefs. The most famous tomb in the valley is that of Nefertari, the beloved wife of Ramses II, known for its vivid and well-preserved artwork. The site provides valuable insights into the burial practices and beliefs of ancient Egyptian royalty. The tombs are less crowded than those in the Valley of the Kings, offering a more intimate experience. The valley's peaceful atmosphere and historical significance make it a must-visit for history enthusiasts.",
         "price": {
             "Entrance Fee": {
@@ -102,7 +102,7 @@ places = [
     {
         "id": 43,
         "name": "Colossi of Memnon",
-        "imagesrc": "",
+        "imagesrc": "https://th.bing.com/th/id/R.5938424af9b54be175f1231c6a36465d?rik=d58QZ1ZRhSffbQ&pid=ImgRaw&r=0",
         "description": "The Colossi of Memnon are two massive stone statues of Pharaoh Amenhotep III, located on the west bank of the Nile River near Luxor. These iconic statues have stood for over 3,400 years and are among the most recognizable landmarks in Egypt. Each statue is about 18 meters high and weighs approximately 720 tons. The Colossi originally guarded the entrance to Amenhotep's mortuary temple, which was one of the largest and most magnificent temples in ancient Egypt. Today, the temple is mostly in ruins, but the Colossi remain an impressive sight. The statues have been a source of fascination for travelers and historians for centuries. They are particularly famous for the 'singing' phenomenon, where one of the statues would emit a musical sound at dawn, a mystery that captivated ancient visitors. The Colossi of Memnon are an enduring symbol of the grandeur of ancient Egyptian civilization.",
         "price": {
             "Entrance Fee": {
@@ -127,7 +127,7 @@ places = [
     {
         "id": 44,
         "name": "Hatshepsut's Mortuary Temple",
-        "imagesrc": "",
+        "imagesrc": "https://th.bing.com/th/id/R.cd6928a999017dce9faf88ba3ec5dd26?rik=kRunuCc8C7GgiQ&pid=ImgRaw&r=0",
         "description": "Hatshepsut's Mortuary Temple, also known as Deir el-Bahari, is an impressive funerary temple built for the female pharaoh Hatshepsut. Located on the west bank of the Nile near Luxor, the temple is renowned for its stunning architecture, including terraces and colonnades that blend seamlessly with the surrounding cliffs. The temple complex is dedicated to the sun god Amun-Ra and features beautiful reliefs depicting Hatshepsut's divine birth and her expedition to the Land of Punt. The temple's innovative design and artistic achievements reflect the high point of ancient Egyptian architecture and craftsmanship. Visitors can explore the various chapels, sanctuaries, and the central courtyard. The temple's serene location and historical significance make it a fascinating site to visit. Hatshepsut's Mortuary Temple stands as a testament to the legacy of one of Egypt's most successful and enigmatic pharaohs.",
         "price": {
             "Entrance Fee": {
@@ -152,7 +152,7 @@ places = [
     {
         "id": 45,
         "name": "Luxor Museum",
-        "imagesrc": "",
+        "imagesrc": "https://i0.wp.com/www.ourpassionfortravel.com/wp-content/uploads/2019/08/Turin-Edits-380.jpg?resize=640%2C427&is-pending-load=1#038;ssl=1",
         "description": "Luxor Museum is a prominent museum located in Luxor, Egypt, showcasing an impressive collection of artifacts from the region. The museum offers a more intimate and focused experience compared to the larger Egyptian Museum in Cairo. Visitors can admire a range of exhibits, including statues, jewelry, pottery, and mummies, all beautifully displayed in a modern and well-lit setting. Highlights include the statues of Amenhotep III and the cache of royal mummies discovered in the nearby Valley of the Kings. The museum's chronological layout provides a comprehensive overview of Luxor's history from the Old Kingdom to the Islamic era. Interactive displays and informative plaques enhance the visitor experience, making it educational and engaging. Luxor Museum is an essential visit for those interested in ancient Egyptian history and culture. The museum also offers stunning views of the Nile River from its outdoor terrace.",
         "price": {
             "Entrance Fee": {
@@ -177,7 +177,7 @@ places = [
     {
         "id": 46,
         "name": "Temple of Seti I",
-        "imagesrc": "",
+        "imagesrc": "https://i.redd.it/gzvdealgc8751.jpg",
         "description": "The Temple of Seti I is an unfinished temple located in Abydos, dedicated to the god Osiris. Known for its beautiful reliefs and inscriptions, the temple offers a fascinating glimpse into ancient Egyptian religious practices. The temple was built by Seti I and later completed by his son, Ramses II. Its unique L-shaped design features several chapels dedicated to different deities, including Osiris, Isis, and Horus. The quality and detail of the carvings are among the finest in Egypt, depicting scenes of the pharaoh's divine birth and his relationship with the gods. The temple also includes the Abydos King List, a chronological list of pharaohs from the earliest times to Seti I. Despite its unfinished state, the Temple of Seti I remains a significant site for its architectural and artistic achievements. Visitors can explore the various chambers and admire the well-preserved decorations that have survived thousands of years.",
         "price": {
             "Entrance Fee": {
@@ -202,7 +202,7 @@ places = [
     {
         "id": 47,
         "name": "Temple of Ramesses II",
-        "imagesrc": "",
+        "imagesrc": "https://foundtheworld.com/wp-content/uploads/2016/03/Abu-Simbel-Temple-5.jpg",
         "description": "The Temple of Ramesses II, located in the Valley of the Kings, is a magnificent structure known for its colossal statues of the pharaoh. Also known as the Ramesseum, this mortuary temple was dedicated to the god Ra and served as a place of worship and remembrance for Ramses II. The temple features a massive pylon, a hypostyle hall, and several sanctuaries. The colossal statues, originally flanking the entrance, are among the most striking features, showcasing the grandeur of Ramses II's reign. The walls of the temple are adorned with intricate reliefs depicting scenes from the Battle of Kadesh and other significant events. Despite the passage of time, the temple's architecture and artwork remain impressive. The site provides valuable insights into the religious and political life of ancient Egypt. Visitors can explore the ruins and appreciate the craftsmanship and scale of this monumental temple.",
         "price": {
             "Entrance Fee": {
@@ -227,7 +227,7 @@ places = [
     {
         "id": 48,
         "name": "Tomb of Tutankhamun",
-        "imagesrc": "",
+        "imagesrc": "https://th.bing.com/th/id/R.649e847763275c9fdf534c433d901f2d?rik=AqW3yAf%2b2rJPpw&pid=ImgRaw&r=0",
         "description": "The Tomb of Tutankhamun, discovered nearly intact by Howard Carter in 1922, is one of the most famous archaeological finds in history. Located in the Valley of the Kings, the tomb of the boy king Tutankhamun is renowned for its wealth of treasures and its relatively small size compared to other royal tombs. The tomb's antechamber, burial chamber, and treasury were filled with stunning artifacts, including the iconic golden mask of Tutankhamun. The discovery of the tomb provided invaluable insights into the burial practices and material culture of ancient Egypt. Visitors to the tomb can see the well-preserved wall paintings and the original stone sarcophagus. The treasures of Tutankhamun are now housed in the Egyptian Museum in Cairo, but the tomb itself remains a significant and popular tourist attraction. The story of its discovery and the legacy of Tutankhamun continue to captivate people worldwide.",
         "price": {
             "Entrance Fee": {
@@ -252,7 +252,7 @@ places = [
     {
         "id": 49,
         "name": "Tomb of Nefertari",
-        "imagesrc": "",
+        "imagesrc": "https://th.bing.com/th/id/R.a4b45c06de6a181f40dc18b64bebf070?rik=2L72mPMLfY2haw&pid=ImgRaw&r=0",
         "description": "The Tomb of Nefertari, located in the Valley of the Queens, is one of the most beautiful and well-preserved tombs in Egypt. Nefertari was the beloved wife of Ramses II, and her tomb reflects her high status and the love her husband had for her. The tomb is renowned for its vivid and colorful wall paintings, depicting Nefertari in various divine and daily life scenes. The quality of the artwork and the preservation of the colors are exceptional, offering a glimpse into the artistry of ancient Egyptian craftsmen. The tomb includes a series of chambers, each adorned with intricate and symbolic decorations. The Tomb of Nefertari is considered one of the finest examples of ancient Egyptian funerary art. Access to the tomb is limited to preserve its delicate condition, making it a rare and exclusive experience for visitors. The tomb's beauty and historical significance make it a highlight of any visit to Luxor.",
         "price": {
             "Entrance Fee": {
@@ -277,7 +277,7 @@ places = [
     {
         "id": 50,
         "name": "Tomb of Thutmose III",
-        "imagesrc": "",
+        "imagesrc": "https://egypt-museum.com/wp-content/uploads/2022/07/Interior-of-the-Tomb-of-Thutmose-III.jpg",
         "description": "The Tomb of Thutmose III, located in the Valley of the Kings, is a uniquely designed tomb featuring a spiral staircase and vibrant wall paintings. Thutmose III, often referred to as the Napoleon of ancient Egypt, was one of the greatest military pharaohs. His tomb reflects his importance and the grandeur of his reign. The tomb is accessed through a steep descent, leading to a series of chambers decorated with scenes from the Amduat, an ancient Egyptian funerary text. The vivid and detailed paintings provide insights into the religious beliefs and practices of the time. The tomb's innovative design and the quality of its artwork make it a significant site in the Valley of the Kings. Despite being plundered in antiquity, the tomb remains an important archaeological and tourist destination. Visitors can explore the chambers and admire the intricate decorations that have survived the ravages of time.",
         "price": {
             "Entrance Fee": {
@@ -302,7 +302,7 @@ places = [
     {
         "id": 51,
         "name": "Tomb of Ramesses VI",
-        "imagesrc": "",
+        "imagesrc": "https://media.odynovotours.com/thumbnail/tour/51000/11588086465848472/ValleyoftheKings1_50715-720w.jpg",
         "description": "The Tomb of Ramesses VI, located in the Valley of the Kings, is known for its detailed astronomical ceiling and colorful wall decorations. The tomb is one of the most beautifully decorated in the valley, showcasing the artistic achievements of ancient Egyptian craftsmen. The ceiling features intricate depictions of the night sky, including constellations and astronomical symbols. The walls are adorned with scenes from various funerary texts, including the Book of the Dead and the Book of Gates. The tomb's layout and design reflect the importance of Ramesses VI and his desire to ensure a safe passage to the afterlife. Despite damage and looting over the centuries, the tomb remains an important and popular site for visitors. The preservation of the colors and the quality of the artwork make it a highlight of any visit to the Valley of the Kings. The tomb provides valuable insights into ancient Egyptian beliefs about the afterlife and the pharaoh's role in the cosmos.",
         "price": {
             "Entrance Fee": {
@@ -327,10 +327,10 @@ places = [
     {
         "id": 52,
         "name": "Hot Air Balloon Ride",
-        "imagesrc": "",
+        "imagesrc": "https://www.kemetexperience.com/wp-content/uploads/2019/09/Luxor-hot-balloon.jpg",
         "description": "A Hot Air Balloon Ride over Luxor offers a unique and breathtaking perspective of the ancient city and its surroundings. The early morning flights provide stunning views of the temples, the Nile River, and the desert landscape as the sun rises. This experience allows visitors to see Luxor from a new angle, capturing the beauty and majesty of the historical sites from above. The ride typically lasts about an hour, during which passengers can enjoy a serene and peaceful journey through the sky. The balloons are operated by experienced pilots, ensuring a safe and enjoyable experience. The views of landmarks such as the Valley of the Kings, Karnak Temple, and Hatshepsut's Mortuary Temple are truly unforgettable. A hot air balloon ride is a must-do activity for those seeking a memorable and unique way to experience Luxor. The combination of adventure and history makes it an ideal addition to any visit to this ancient city.",
         "price": {
-            "Entrance Fee": {
+            "Prices": {
                 "Egyptian": 200,
                 "Student": 150,
                 "Foreign": 400,
@@ -352,7 +352,7 @@ places = [
     {
         "id": 53,
         "name": "Dendera Temple Complex",
-        "imagesrc": "",
+        "imagesrc": "https://th.bing.com/th/id/R.811e42da0a335d2a6a9cb143c1ae5ed9?rik=QbSEes%2bQ3D4NEQ&pid=ImgRaw&r=0",
         "description": "The Dendera Temple Complex, located north of Luxor, is one of the best-preserved temple sites in Egypt. Dedicated to the goddess Hathor, the complex features a stunning zodiac ceiling, which is one of its most famous attractions. The main temple, the Temple of Hathor, showcases elaborate carvings and well-preserved reliefs depicting scenes of the pharaohs and deities. The complex also includes the Temple of Isis, the Sacred Lake, and several smaller chapels and shrines. The detailed astronomical and astrological decorations provide valuable insights into ancient Egyptian beliefs and practices. The temple's impressive architecture and intricate artwork make it a significant historical and cultural site. Visitors can explore the various chambers and admire the craftsmanship that has survived thousands of years. The Dendera Temple Complex offers a fascinating glimpse into the religious and artistic achievements of ancient Egypt, making it a must-visit for history enthusiasts.",
         "price": {
             "Entrance Fee": {
