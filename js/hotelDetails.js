4ade5515b1976464f5833d5ec5a842e12522737f
Rawan <rawanehab523@gmail.com>
2024-08-02 04:21:05 +0300

    added cairo hotels

--- js/hotelDetails.js
+++ js/hotelDetails.js
@@ -7,134 +7,147 @@ Hotels = [
         outsideimg: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/456051765.jpg?k=03bf475a247082e40c31debabffde61ec61d622961979e383eefd689dec8a2c9&o=&hp=1",
         description: "The Nile Ritz-Carlton, Cairo is a luxurious hotel offering stunning views of the Nile River. The hotel features elegant rooms and suites, each designed with modern amenities and plush furnishings. Guests can indulge in a variety of dining options, from international cuisine to local delicacies. The hotel boasts a state-of-the-art spa, providing a range of treatments and therapies. A fitness center and a beautiful outdoor pool are available for guests to enjoy. Business travelers can take advantage of the hotel's extensive meeting and event facilities. The Nile Ritz-Carlton, Cairo is the perfect choice for those seeking luxury and comfort in the heart of Cairo.",
         prices: "$300 - $500 per night",
-        link: "ana linkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk"
+        link: "https://www.booking.com/hotel/eg/the-nile-ritz-carlton-cairo.html"
     },
     {
         id: 2,
         name: "Four Seasons Hotel Cairo at Nile Plaza",
         city: "cairo",
-        insideimg: "",
-        outsideimg: "",
+        insideimg: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/471075248.jpg?k=d47481c2effda4f09ca11f5a7179929c1017ffeef9441d23c84441bd39bd8029&o=&hp=1",
+        outsideimg: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/94190040.jpg?k=ef1c7c9927dbc4c9bb8ade64114c12302c1490a01530fec267460efffb8fdf47&o=&hp=1",
         description: "Four Seasons Hotel Cairo at Nile Plaza offers an elegant retreat with breathtaking views of the Nile River. The hotel features spacious and stylish rooms, equipped with modern amenities and luxurious furnishings. Guests can dine at a variety of restaurants, offering both international and local cuisine. The hotel includes a rooftop pool, providing a relaxing oasis in the heart of the city. A full-service spa is available, offering a range of treatments to rejuvenate the mind and body. Business facilities are top-notch, catering to the needs of corporate travelers. With its prime location and exceptional service, Four Seasons Hotel Cairo at Nile Plaza is a top choice for visitors to Cairo.",
         prices: "$350 - $600 per night",
-        link: ""
+        link: "https://www.booking.com/hotel/eg/four-seasons-cairo-at-nile-plaza.html"
     },
     {
         id: 3,
         name: "Kempinski Nile Hotel Garden City Cairo",
-        insideimg: "",
-        outsideimg: "",
+        city: "cairo",
+        insideimg: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/543472456.jpg?k=eee0456e05c6abc4926df2c0ee8b6f005ee9ac6415e736c5f998354ef331f1e5&o=&hp=1",
+        outsideimg: "https://hi-cdn.t-rp.co.uk/images/hotels/2046320/0?width=870&height=480&crop=false",
         description: "Kempinski Nile Hotel Garden City Cairo is a boutique hotel that offers personalized service and luxurious accommodations. The hotel features elegantly designed rooms and suites, each equipped with modern amenities. Guests can enjoy a variety of dining options, from gourmet restaurants to casual cafes. The rooftop pool provides stunning views of the Nile River and the city skyline. The hotel also offers a state-of-the-art fitness center and a full-service spa. Business travelers will appreciate the well-equipped meeting rooms and event spaces. Kempinski Nile Hotel Garden City Cairo is the ideal choice for those seeking a luxurious and intimate stay in Cairo.",
         prices: "$200 - $400 per night",
-        link: ""
+        link: "https://www.booking.com/hotel/eg/kempinski-nile-cairo.html"
     },
     {
         id: 4,
         name: "Sofitel Cairo Nile El Gezirah",
-        insideimg: "",
-        outsideimg: "",
+        city: "cairo",
+        insideimg: "https://dq5r178u4t83b.cloudfront.net/wp-content/uploads/sites/23/2019/03/05093021/Room-20071web.jpg",
+        outsideimg: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/495299436.jpg?k=672263f91543fdd0bdcca4e85737d98414a5f1ea91d5b95ec72fd09ad8135290&o=&hp=1",
         description: "Sofitel Cairo Nile El Gezirah is a luxury hotel located on an island in the Nile River, offering a unique and tranquil setting. The hotel features elegantly appointed rooms and suites, each with modern amenities and beautiful views. Guests can dine at a variety of restaurants, serving international and local cuisine. The hotel boasts a beautiful outdoor pool and a full-service spa, providing a relaxing retreat. A fitness center is available for guests who wish to stay active during their stay. Business facilities are extensive, catering to the needs of corporate travelers. Sofitel Cairo Nile El Gezirah is the perfect choice for those seeking a luxurious and peaceful stay in Cairo.",
         prices: "$250 - $450 per night",
-        link: ""
+        link: "https://www.booking.com/hotel/eg/el-gezirah-cairo.html"
     },
     {
         id: 5,
         name: "Conrad Cairo",
-        insideimg: "",
-        outsideimg: "",
+        city: "cairo",
+        insideimg: "https://static21.com-hotel.com/uploads/hotel/60789/photo/conrad-cairo-hotel-casino_153824959610.jpg",
+        outsideimg: "https://www.fivestaralliance.com/files/fivestaralliance.com/field/image/nodes/2009/11323/1507_0_conradcairo_fsa-g.jpg",
         description: "Conrad Cairo is a luxury hotel located in the heart of Cairo, offering stunning views of the Nile River. The hotel features spacious and elegantly designed rooms and suites, each equipped with modern amenities. Guests can enjoy a variety of dining options, from international cuisine to local specialties. The hotel includes a beautiful outdoor pool and a state-of-the-art fitness center. A full-service spa is available, offering a range of treatments to relax and rejuvenate. Business travelers can take advantage of the hotel's extensive meeting and event facilities. Conrad Cairo is the ideal choice for those seeking luxury and convenience in the heart of Cairo.",
         prices: "$200 - $400 per night",
-        link: ""
+        link: "https://www.booking.com/hotel/eg/conrad-cairo.html"
     },
     {
         id: 6,
-        name: "Marriott Mena House, Cairo",
-        insideimg: "",
-        outsideimg: "",
+        name: "Marriott Mena House",
+        city: "cairo",
+        insideimg: "https://cache.marriott.com/content/dam/marriott-renditions/CAIMN/caimn-guestroom-5655-hor-clsc.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=1336px:*",
+        outsideimg: "https://nohomejustroam.com/wp-content/uploads/2021/08/Marriott-Mena-House-in-Cairo-Egypt.jpg",
         description: "Marriott Mena House, Cairo is a historic luxury hotel located near the Giza Pyramids, offering stunning views of these ancient wonders. The hotel features elegantly appointed rooms and suites, each equipped with modern amenities. Guests can dine at a variety of restaurants, serving international and local cuisine. The hotel includes a beautiful outdoor pool and a state-of-the-art fitness center. A full-service spa is available, offering a range of treatments to relax and rejuvenate. The hotel's lush gardens provide a tranquil setting for guests to enjoy. Marriott Mena House, Cairo is the perfect choice for those seeking luxury and history in one location.",
         prices: "$250 - $500 per night",
-        link: ""
+        link: "https://www.booking.com/hotel/eg/mena-house-oberoi.html"
     },
     {
         id: 7,
         name: "Fairmont Nile City Hotel",
-        insideimg: "",
-        outsideimg: "",
+        city: "cairo",
+        insideimg: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/12/1e/ea/e5/deluxe-nile-room.jpg?w=700&h=-1&s=1",
+        outsideimg: "https://content.skyscnr.com/available/1329313207/1329313207_WxH.jpg",
         description: "Fairmont Nile City Hotel is a luxury hotel located on the banks of the Nile River, offering stunning views of the river and the city. The hotel features spacious and elegantly designed rooms and suites, each equipped with modern amenities. Guests can enjoy a variety of dining options, from international cuisine to local specialties. The hotel includes a beautiful outdoor pool and a state-of-the-art fitness center. A full-service spa is available, offering a range of treatments to relax and rejuvenate. Business travelers can take advantage of the hotel's extensive meeting and event facilities. Fairmont Nile City Hotel is the ideal choice for those seeking luxury and convenience in Cairo.",
         prices: "$200 - $400 per night",
-        link: ""
+        link: "https://www.booking.com/hotel/eg/fairmont-nile-city.html"
     },
     {
         id: 8,
         name: "Hilton Cairo Zamalek Residences",
-        insideimg: "",
-        outsideimg: "",
+        city: "cairo",
+        insideimg: "https://www.hilton.com/im/en/CAIZRHI/18872529/hilton-zamalek4823-copy.jpg?impolicy=crop&cw=5000&ch=2799&gravity=NorthWest&xposition=0&yposition=266&rw=768&rh=430",
+        outsideimg: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/483731223.jpg?k=a15814150134a62fb8ac4ddad79d4da124f3f7af39c2c2d15082eb3f76bb15e0&o=&hp=1",
         description: "Hilton Cairo Zamalek Residences is a luxury hotel located on the island of Zamalek, offering stunning views of the Nile River. The hotel features spacious and elegantly designed rooms and suites, each equipped with modern amenities. Guests can enjoy a variety of dining options, from international cuisine to local specialties. The hotel includes a beautiful outdoor pool and a state-of-the-art fitness center. A full-service spa is available, offering a range of treatments to relax and rejuvenate. Business travelers can take advantage of the hotel's extensive meeting and event facilities. Hilton Cairo Zamalek Residences is the ideal choice for those seeking luxury and tranquility in Cairo.",
         prices: "$150 - $300 per night",
-        link: ""
+        link: "https://www.booking.com/hotel/eg/hilton-zamalek-residence.html"
     },
     {
         id: 9,
         name: "InterContinental Cairo Semiramis",
-        insideimg: "",
-        outsideimg: "",
+        city: "cairo",
+        insideimg: "https://digital.ihg.com/is/image/ihg/intercontinental-cairo-7885275924-4x3",
+        outsideimg: "https://digital.ihg.com/is/image/ihg/intercontinental-cairo-4533898720-16x9?",
         description: "InterContinental Cairo Semiramis is a luxury hotel located in the heart of Cairo, offering stunning views of the Nile River. The hotel features spacious and elegantly designed rooms and suites, each equipped with modern amenities. Guests can enjoy a variety of dining options, from international cuisine to local specialties. The hotel includes a beautiful outdoor pool and a state-of-the-art fitness center. A full-service spa is available, offering a range of treatments to relax and rejuvenate. Business travelers can take advantage of the hotel's extensive meeting and event facilities. InterContinental Cairo Semiramis is the ideal choice for those seeking luxury and convenience in Cairo.",
         prices: "$200 - $400 per night",
-        link: ""
+        link: "https://www.booking.com/hotel/eg/intercontinental-cairo-semiramis.html"
     },
     {
         id: 10,
         name: "Steigenberger Hotel El Tahrir",
-        insideimg: "",
-        outsideimg: "",
+        city: "cairo",
+        insideimg: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/522974545.jpg?k=7e9ef64283f3852adea5186bbf1d5be3b1db2fb45802ca6c207f63e1d976b8c3&o=&hp=1",
+        outsideimg: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/484416516.jpg?k=257d19b577287cd83862ec27b93a6dda9bd5d99b7a6cb0b466b633b61521de77&o=&hp=1",
         description: "Steigenberger Hotel El Tahrir is a luxury hotel located in the heart of Cairo, offering easy access to the city's main attractions. The hotel features spacious and elegantly designed rooms and suites, each equipped with modern amenities. Guests can enjoy a variety of dining options, from international cuisine to local specialties. The hotel includes a beautiful outdoor pool and a state-of-the-art fitness center. A full-service spa is available, offering a range of treatments to relax and rejuvenate. Business travelers can take advantage of the hotel's extensive meeting and event facilities. Steigenberger Hotel El Tahrir is the ideal choice for those seeking luxury and convenience in Cairo.",
         prices: "$150 - $300 per night",
-        link: ""
+        link: "https://www.booking.com/hotel/eg/steigenberger-el-tahrir.html"
     },
     {
         id: 11,
         name: "Novotel Cairo El Borg",
-        insideimg: "",
-        outsideimg: "",
+        city: "cairo",
+        insideimg: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/359875831.jpg?k=85bfe6fcd07c00cb6da1a8aabb8d8032e9bea67fd468aa4b9a8366f8a570a3de&o=&hp=1",
+        outsideimg: "https://www.hotel.de/de/media/image/a1/dd/c9/Novotel_Cairo_El_Borg-Kairo-Aussenansicht-12-418854.jpg",
         description: "Novotel Cairo El Borg is a modern hotel located on the banks of the Nile River, offering stunning views of the river and the city. The hotel features spacious and elegantly designed rooms and suites, each equipped with modern amenities. Guests can enjoy a variety of dining options, from international cuisine to local specialties. The hotel includes a beautiful outdoor pool and a state-of-the-art fitness center. A full-service spa is available, offering a range of treatments to relax and rejuvenate. Business travelers can take advantage of the hotel's extensive meeting and event facilities. Novotel Cairo El Borg is the ideal choice for those seeking luxury and convenience in Cairo.",
         prices: "$100 - $250 per night",
-        link: ""
+        link: "https://www.booking.com/hotel/eg/novotel-cairo-el-borg.html"
     },
     {
         id: 12,
         name: "Pyramisa Suites Hotel Cairo",
-        insideimg: "",
-        outsideimg: "",
+        city: "cairo",
+        insideimg: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/206959369.jpg?k=af40bcf2465a7bc94f3af928e802481e262ec23132579a1f54c718396652f181&o=&hp=1",
+        outsideimg: "https://i.ytimg.com/vi/2ybJAOII6mU/sddefault.jpg",
         description: "Pyramisa Suites Hotel Cairo is a luxury hotel located in the heart of Cairo, offering easy access to the city's main attractions. The hotel features spacious and elegantly designed rooms and suites, each equipped with modern amenities. Guests can enjoy a variety of dining options, from international cuisine to local specialties. The hotel includes a beautiful outdoor pool and a state-of-the-art fitness center. A full-service spa is available, offering a range of treatments to relax and rejuvenate. Business travelers can take advantage of the hotel's extensive meeting and event facilities. Pyramisa Suites Hotel Cairo is the ideal choice for those seeking luxury and convenience in Cairo.",
         prices: "$100 - $250 per night",
-        link: ""
+        link: "https://www.booking.com/hotel/eg/pyramisa-suites-casino-cairo.html"
     },
     {
         id: 13,
         name: "Ramses Hilton",
-        insideimg: "",
-        outsideimg: "",
+        city: "cairo",
+        insideimg: "https://www.hilton.com/im/en/CAIRHTW/15122444/executive-king-room-nile-view-.jpg?impolicy=crop&cw=4500&ch=2519&gravity=NorthWest&xposition=0&yposition=240&rw=768&rh=430",
+        outsideimg: "https://www.hilton.com/im/en/CAIRHTW/16763081/exterior-night-shot.jpg?impolicy=crop&cw=5000&ch=2799&gravity=NorthWest&xposition=0&yposition=267&rw=768&rh=430",
         description: "Ramses Hilton is a luxury hotel located in the heart of Cairo, offering stunning views of the Nile River. The hotel features spacious and elegantly designed rooms and suites, each equipped with modern amenities. Guests can enjoy a variety of dining options, from international cuisine to local specialties. The hotel includes a beautiful outdoor pool and a state-of-the-art fitness center. A full-service spa is available, offering a range of treatments to relax and rejuvenate. Business travelers can take advantage of the hotel's extensive meeting and event facilities. Ramses Hilton is the ideal choice for those seeking luxury and convenience in Cairo.",
         prices: "$150 - $300 per night",
-        link: ""
+        link: "https://www.booking.com/hotel/eg/ramses-hilton.html"
     },
     {
         id: 14,
         name: "Sheraton Cairo Hotel & Casino",
-        insideimg: "",
-        outsideimg: "",
+        city: "cairo",
+        insideimg: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/481582832.jpg?k=2474951fed695db5fdce19ddc9bccf091b454c15c58359b176eca4d2f461c47e&o=&hp=1",
+        outsideimg: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/481582622.jpg?k=627e83a9a23ed5b823687c9b1b119276618fb43f3435bebbfb485c2a48f4fc93&o=&hp=1",
         description: "Sheraton Cairo Hotel & Casino is a luxury hotel located on the banks of the Nile River, offering stunning views of the river and the city. The hotel features spacious and elegantly designed rooms and suites, each equipped with modern amenities. Guests can enjoy a variety of dining options, from international cuisine to local specialties. The hotel includes a beautiful outdoor pool and a state-of-the-art fitness center. A full-service spa is available, offering a range of treatments to relax and rejuvenate. The hotel's casino provides entertainment for guests looking for some excitement. Sheraton Cairo Hotel & Casino is the ideal choice for those seeking luxury and entertainment in Cairo.",
         prices: "$200 - $400 per night",
-        link: ""
+        link: "https://www.booking.com/hotel/eg/shecairoeg.html"
     },
     {
         id: 15,
         name: "Le Meridien Cairo Airport",
-        insideimg: "",
-        outsideimg: "",
+        city: "cairo",
+        insideimg: "https://cache.marriott.com/content/dam/marriott-renditions/CAIAM/caiam-twin-guestroom-5561-hor-clsc.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=1336px:*",
+        outsideimg: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/83/30/00/le-meridien-cairo-airport.jpg?w=700&h=-1&s=1",
         description: "Le Meridien Cairo Airport is a modern hotel located adjacent to Cairo International Airport, offering convenience for travelers. The hotel features spacious and elegantly designed rooms and suites, each equipped with modern amenities. Guests can enjoy a variety of dining options, from international cuisine to local specialties. The hotel includes a beautiful outdoor pool and a state-of-the-art fitness center. A full-service spa is available, offering a range of treatments to relax and rejuvenate. Business travelers can take advantage of the hotel's extensive meeting and event facilities. Le Meridien Cairo Airport is the ideal choice for those seeking luxury and convenience near the airport.",
         prices: "$150 - $300 per night",
-        link: ""
+        link: "https://www.booking.com/hotel/eg/le-meridien-cairo-airport.html"
     }
 ];
 
