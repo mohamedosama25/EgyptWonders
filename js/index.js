8abcd7eb737dc91d9120b2e35537c01daf9f2514
mohamedosama25 <147179514+mohamedosama25@users.noreply.github.com>
2024-08-06 22:18:35 +0300

    homepage done pt1
    added attractive places
    
    Co-Authored-By: Rawan <rawanehab523@gmail.com>

---
+++ js/index.js
@@ -0,0 +1,239 @@
+places = [
+    {
+        "id": 1,
+        "name": "The Pyramids of Giza",
+        "imagesrc": "https://www.ancientworldwonders.com/uploads/Pyramids_of_Giza.jpg",
+        "description": "The Pyramids of Giza are the most iconic symbols of ancient Egypt and are located on the Giza Plateau, just outside Cairo. Comprising three main pyramids—the Great Pyramid of Khufu (Cheops), the Pyramid of Khafre (Chephren), and the Pyramid of Menkaure (Mykerinos)—this site is one of the Seven Wonders of the Ancient World. The Great Pyramid, originally standing at 146.6 meters, was the tallest man-made structure in the world for over 3,800 years. Nearby, the Sphinx, a colossal limestone statue with the body of a lion and the head of a pharaoh, guards the pyramids. These structures, built over 4,500 years ago, were constructed as elaborate tombs for pharaohs and are a testament to the architectural and engineering prowess of ancient Egyptian civilization.",
+        "price": {
+            "Pyramids of Giza": {
+                "Egyptian": 60,
+                "Student": 30,
+                "Foreign": 150,
+                "Foreign Student": 75
+            },
+            "Pyramid of Khufu": {
+                "Egyptian": 100,
+                "Student": 50,
+                "Foreign": 300,
+                "Foreign Student": 150
+            },
+            "Pyramid of Menkaure": {
+                "Egyptian": 30,
+                "Student": 10,
+                "Foreign": 100,
+                "Foreign Student": 50
+            },
+            "Workers' Cemetery": {
+                "Egyptian": 10,
+                "Student": 5,
+                "Foreign": 100,
+                "Foreign Student": 50
+            },
+            "Giza Necropolis Parking Ticket": {
+                "Car": 20,
+                "Minibus": 40,
+                "Bus": 60
+            }
+        },
+        "hours": "7:00 AM to 5:00 PM",
+        "hotels": [
+            {
+                "name": "Marriott Mena House, Cairo",
+                "price": "$272 - $421 per night"
+            },
+            {
+                "name": "Steigenberger Pyramids Cairo",
+                "price": "$140 - $260 per night"
+            },
+            {
+                "name": "Pyramids View Inn",
+                "price": "$50 - $100 per night"
+            }
+        ]
+    },
+    {
+        "id": 18,
+        "name": "Montaza Palace and Gardens",
+        "imagesrc": "https://www.islamicarchitecturalheritage.com/wp-content/uploads/2020/06/montaza-palace.jpg",
+        "description": "Montaza Palace and Gardens are located along the Mediterranean coast. The palace was built in the early 20th century by Khedive Abbas II as a summer residence. The architectural style of the palace combines Ottoman and Florentine influences, making it a unique landmark. The surrounding gardens, covering about 370 acres, are beautifully landscaped with a variety of trees, flowers, and walking paths. The gardens also feature several picturesque beaches and a small marina. Montaza Palace and Gardens are a popular spot for both locals and tourists, offering a serene escape from the bustling city.",
+        "price": {
+            "Prices": {
+                "Egyptian": 25,
+                "Student": 15,
+                "Foreign": 75,
+                "Foreign Student": 50
+            }
+        },
+        "hours": "8:00 AM to 5:00 PM",
+        "hotels": [
+            {
+                "name": "Helnan Palestine Hotel",
+                "price": "$120 - $200 per night"
+            },
+            {
+                "name": "Sheraton Montazah Hotel",
+                "price": "$100 - $180 per night"
+            }
+        ]
+    },
+    {
+        "id": 41,
+        "name": "Valley of the Kings",
+        "imagesrc": "https://www.historyhit.com/app/uploads/fly-images/5166545/shutterstock_valley-of-kings-788x537.jpg",
+        "description": "The Valley of the Kings is a royal burial ground for pharaohs and nobles, located on the west bank of the Nile near Luxor. It is renowned for its elaborately decorated tombs, including the famous tomb of Tutankhamun. The valley contains 63 tombs and chambers, showcasing intricate hieroglyphs and stunning wall paintings. Each tomb provides unique insights into ancient Egyptian beliefs about the afterlife. Visitors can explore several tombs, each with its distinct features and artwork. The valley's isolation and hidden location were intended to protect the tombs from looters. Despite this, many tombs were plundered in antiquity. Today, it remains a major archaeological and tourist site, attracting researchers and history enthusiasts worldwide.",
+        "price": {
+            "Entrance Fee": {
+                "Egyptian": 60,
+                "Student": 30,
+                "Foreign": 240,
+                "Foreign Student": 120
+            }
+        },
+        "hours": "6:00 AM to 5:00 PM",
+        "hotels": [
+            {
+                "name": "Maritim Jolie Ville Kings Island Luxor",
+                "price": "$140 - $280 per night"
+            },
+            {
+                "name": "Hilton Luxor Resort & Spa",
+                "price": "$180 - $350 per night"
+            }
+        ]
+    },
+    {
+        "id": 61,
+        "name": "Abu Simbel Temples",
+        "imagesrc": "https://res.cloudinary.com/culturemap-com/image/upload/ar_4:3,c_fill,g_faces:center,w_980/v1628778964/photos/322989_original.jpg",
+        "description": "The Abu Simbel Temples are massive rock temples located near the Sudanese border, about 280 km from Aswan. Built by Ramses II, the temples are dedicated to the gods Ra-Horakhty, Amun, and Ptah, as well as Ramses II himself. The larger temple features four colossal statues of Ramses II, while the smaller temple is dedicated to his queen, Nefertari. The temples were relocated in the 1960s to save them from the rising waters of Lake Nasser. The intricate carvings and impressive architecture make the temples a UNESCO World Heritage Site. Visitors can explore the temples and learn about their historical and cultural significance.",
+        "price": {
+            "Prices":{
+            "Adults": "240 EGP",
+            "Students": "120 EGP"
+            }
+            
+        },
+        "hours": "5:00 AM to 6:00 PM",
+        "hotels": [
+            {
+                "name": "Seti Abu Simbel Lake Resort",
+                "price": "$200 - $350 per night"
+            },
+            {
+                "name": "Nefertari Hotel Abu Simbel",
+                "price": "$150 - $300 per night"
+            }
+        ]
+    },
+    {
+        "id": 67,
+        "name": "Tiran Island",
+        "imagesrc": "https://media.tacdn.com/media/attractions-splice-spp-674x446/06/d0/10/24.jpg",
+        "description": "Tiran Island is a must-visit for snorkeling and diving enthusiasts, known for its crystal-clear waters and abundant underwater life. Located at the entrance of the Gulf of Aqaba, the island is surrounded by stunning coral reefs teeming with vibrant marine species. Snorkelers and divers can explore the underwater paradise, encountering colorful fish, sea turtles, and even the occasional shark. The island itself offers pristine beaches and tranquil surroundings, making it a perfect spot for relaxation. Boat trips to Tiran Island are a popular activity, providing a full day of adventure and exploration. The island's unique ecosystem and natural beauty make it a highlight of the Red Sea region. Tiran Island is an unmissable destination for nature lovers and water sports enthusiasts alike.",
+        "price": {
+            "Enrance" : "Free"
+        },
+        "hours": "10:00 AM to 6:00 PM",
+        "hotels": [
+            {
+                "name": "Rixos Premium Seagate",
+                "price": "$200 - $400 per night"
+            },
+            {
+                "name": "Sultan Gardens Resort",
+                "price": "$150 - $300 per night"
+            }
+        ]
+    },
+    {
+        "id": 81,
+        "name": "Shali Fortress",
+        "imagesrc": "https://i.pinimg.com/originals/dc/e0/38/dce038b4f536c0959167a1566d75886d.jpg",
+        "description": "Shali Fortress is an ancient mud-brick fortress that offers panoramic views of the oasis and surrounding desert. The fortress, built in the 13th century, served as a protective citadel for the inhabitants of Siwa. It is a great place to explore the history and architecture of the area. The labyrinth of narrow alleys, crumbling walls, and ancient ruins evoke a sense of adventure and history. Shali Fortress is constructed from kershef, a traditional material made from salt and clay, which gives it its unique appearance. Over time, the fortress has weathered natural elements, creating a dramatic and evocative landscape. Visitors can climb to the top of the fortress for breathtaking views and to imagine the daily lives of its original inhabitants. It is an essential site for anyone interested in Siwa's rich heritage and stunning desert vistas.",
+        "price": {
+            "Entrance Fee": {
+                "Egyptian": 30,
+                "Student": 15,
+                "Foreign": 100,
+                "Foreign Student": 50
+            }
+        },
+        "hours": "8:00 AM to 6:00 PM",
+        "hotels": [
+            {
+                "name": "Siwa Shali Resort",
+                "price": "$90 - $150 per night"
+            },
+            {
+                "name": "Adrere Amellal",
+                "price": "$180 - $300 per night"
+            },
+            {
+                "name": "Taziry Ecolodge",
+                "price": "$120 - $200 per night"
+            }
+        ]
+    },
+    {
+        "id": 95,
+        "name": "Wadi El Rayan",
+        "imagesrc": "https://d3rr2gvhjw0wwy.cloudfront.net/uploads/activity_galleries/50720/900x600-1-50-092e7dddb7dbe11549ee18b62967b01f.jpg",
+        "description": "Wadi El Rayan is a stunning protected area known for its two man-made lakes connected by Egypt's only waterfalls. Located in the Fayoum desert, it offers a unique blend of natural beauty and recreational activities. Visitors can enjoy boat rides, bird watching, and even sandboarding on the surrounding dunes. The serene environment and picturesque landscapes make it an ideal spot for nature lovers and photographers. Wadi El Rayan is also home to various wildlife species, including rare migratory birds.",
+        "price": {
+            "Entrance Fee": {
+                "Egyptian": 10,
+                "Student": 5,
+                "Foreign": 40,
+                "Foreign Student": 20
+            }
+        },
+        "hours": "7:00 AM to 5:00 PM",
+        "hotels": [
+            {
+                "name": "Helnan Auberge Fayoum",
+                "price": "$100 - $200 per night"
+            },
+            {
+                "name": "Tunis Village Ecolodge",
+                "price": "$60 - $120 per night"
+            }
+        ]
+    },
+]
+
+function createCards(places) {
+    const container = document.getElementById('cards-container');
+    places.forEach(place => {
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        const img = document.createElement('img');
+        img.setAttribute('src', place.imagesrc);
+        img.setAttribute('alt', place.name);
+
+        const contentDiv = document.createElement('div');
+        contentDiv.className = 'card__content';
+
+        const title = document.createElement('p');
+        title.className = 'card__title';
+        title.textContent = place.name;
+
+        const description = document.createElement('p');
+        description.className = 'card__description';
+        description.textContent = place.description;
+
+        contentDiv.appendChild(title);
+        contentDiv.appendChild(description);
+
+        card.appendChild(img);
+        card.appendChild(contentDiv);
+
+        card.addEventListener('click', () => {
+            window.location.href = `pageDetails.html?id=${place.id}`;
+        });
+
+        container.appendChild(card);
+    });
+};
+
+createCards(places);
